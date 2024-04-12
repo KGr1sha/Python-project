cae000a1750d207d4b80086e2ffb65744cfaf44e
KGr1sha <krylov.grigory@gmail.com>
2024-04-12 17:07:16 +0300

    add scatter plot

--- scripts/data_visualizer.py
+++ scripts/data_visualizer.py
@@ -19,7 +19,6 @@ def show_survival_by_gender():
     plt.ylabel('Survived')
     plt.title('Survival by Gender')
     plt.xticks(rotation=45)
-    plt.tight_layout()
     plt.savefig(os.path.join(SAVE_DIR, 'clustered_bar_plot.png'))
     plt.show()
 
@@ -31,7 +30,6 @@ def show_age_distribution_by_survival():
     plt.ylabel('Frequency')
     plt.title('Histogram of Scores. Age Distribution by Survival')
     plt.legend()
-    plt.tight_layout()
     plt.savefig(os.path.join(SAVE_DIR, 'categorized_histogram_quant_qual.png'))
     plt.show()
 
@@ -43,7 +41,6 @@ def show_boxplot_fare_by_survival():
     plt.title('Boxplot of Fare Distribution by Survival', fontsize=12)
     plt.xticks(ticks=[1, 2], labels=['Not Survived', 'Survived'])
     plt.tick_params(axis='both', which='major', labelsize=10)
-    plt.tight_layout()
     plt.savefig(os.path.join(SAVE_DIR, 'categorized_boxplot_fare.png'))
     plt.show()
 
@@ -55,14 +52,28 @@ def show_boxplot_age_by_survival():
     plt.title('Boxplot of Fare Distribution by Survival', fontsize=12)
     plt.xticks(ticks=[1, 2], labels=['Not Survived', 'Survived'])
     plt.tick_params(axis='both', which='major', labelsize=10)
-    plt.tight_layout()
     plt.savefig(os.path.join(SAVE_DIR, 'categorized_boxplot_age.png'))
     plt.show()
 
 
+def show_scatter_fare_age():
+    plt.figure(figsize=(16, 8))
+    colors = pd.factorize(data['survived'])[0]
+    plt.scatter(data['fare'], data['age'], c=colors, cmap='viridis', alpha=0.6)
+    plt.xlabel('Fare')
+    plt.ylabel('Age')
+    plt.title('Categorized Scatterplot: Fare vs Age by Survived')
+    plt.colorbar(label='Survived')
+    plt.savefig(os.path.join(SAVE_DIR, 'scatterplot_fare_age_survived'))
+    plt.show()
+
+
 if __name__ == '__main__':
+    """
     show_survival_by_gender()
     show_age_distribution_by_survival()
     show_boxplot_fare_by_survival()
     show_boxplot_age_by_survival()
+    """
+    show_scatter_fare_age()
 
